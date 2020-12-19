766b63c935b7a065f8880baaa21483d64eb748e1
musigma <namita.dhakad@effem.com>
2020-12-19 23:31:26 +0530

    Changed filter CSS

--- components/layout.js
+++ components/layout.js
@@ -3,7 +3,7 @@ import styles from "../styles/Home.module.css";
 export default function Layout (props) {
     return(
         <div>
-            <h1>Space X Launch Details</h1>
+            <h1>Space X Launch Programs</h1>
             {props.children}
             <footer className={styles.footer}>
                 <a
@@ -11,7 +11,7 @@ export default function Layout (props) {
                     target="_blank"
                     rel="noopener noreferrer"
                 >
-                    Developed by Namita Dhakad
+                    Developed by: Namita Dhakad
                 </a>
             </footer>
         </div>
--- components/sidePanel.js
+++ components/sidePanel.js
@@ -32,6 +32,8 @@ export default function SidePanel({onApplyFilterHandler, filter, setSpinner}) {
         {label: "2019", value: 2019},
         {label: "2020", value: 2020},
     ];
+
+    const yearButtons = ["2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017", "2018", "2019", "2020"];
     const [year, setYear] = React.useState(null);
     const [launchSuccess, setLaunchSuccess] = React.useState(null);
     const [landingSuccess, setLandingSuccess] = React.useState(null);
@@ -60,12 +62,25 @@ export default function SidePanel({onApplyFilterHandler, filter, setSpinner}) {
     };
 
     const onClickResetHandler = () => {
+        if(year !== null && year !== undefined){
+            const elem2 = document.getElementById("Launch Year_"+year);
+            elem2.classList.remove("btnClassActive");
+            elem2.classList.add("btnClass");
+        }
+        if(launchSuccess !== null && launchSuccess !== undefined){
+            const elem2 = document.getElementById("Launch Success_"+launchSuccess);
+            elem2.classList.remove("btnClassActive");
+            elem2.classList.add("btnClass");
+        }
+        if(landingSuccess !== null && landingSuccess !== undefined){
+            const elem2 = document.getElementById("Landing Success_"+landingSuccess);
+            elem2.classList.remove("btnClassActive");
+            elem2.classList.add("btnClass");
+        }
         setYear(null);
         setLaunchSuccess(null);
         setLandingSuccess(null);
         onClickApplyHandler(null, true);
-        //onUpdateFilter("reset",null);
-        //onLoadLaunchProjects();
     };
 
     async function onClickApplyHandler(e = null, resetFilter = false) {
@@ -115,74 +130,115 @@ export default function SidePanel({onApplyFilterHandler, filter, setSpinner}) {
         setSpinner(false);
     }
 
+    const buttonSelected = (event) => {
+
+        const [filterType, filterValue] = event.target.id.split("_");
+
+        const elem = document.getElementById(event.target.id);
+        if(filterType === "Launch Year"){
+            if(year === filterValue){
+                setYear(null);
+                elem.classList.remove("btnClassActive");
+                elem.classList.add("btnClass");
+            }
+            else{
+                if(year !== null && year !== undefined){
+                    const elem2 = document.getElementById("Launch Year_"+year);
+                    elem2.classList.remove("btnClassActive");
+                    elem2.classList.add("btnClass");
+                }
+                setYear(filterValue);
+                elem.classList.remove("btnClass");
+                elem.classList.add("btnClassActive");
+            }
+        }else if(filterType === "Launch Success"){
+            if(launchSuccess === filterValue){
+                setLaunchSuccess(null);
+                elem.classList.remove("btnClassActive");
+                elem.classList.add("btnClass");
+            }
+            else{
+                if(launchSuccess !== null && launchSuccess !== undefined){
+                    const elem2 = document.getElementById("Launch Success_"+launchSuccess);
+                    elem2.classList.remove("btnClassActive");
+                    elem2.classList.add("btnClass");
+                }
+                setLaunchSuccess(filterValue);
+                elem.classList.remove("btnClass");
+                elem.classList.add("btnClassActive");
+            }
+        }else if(filterType === "Landing Success"){
+            if(landingSuccess === filterValue){
+                setLandingSuccess(null);
+                elem.classList.remove("btnClassActive");
+                elem.classList.add("btnClass");
+            }
+            else{
+                if(landingSuccess !== null && landingSuccess !== undefined){
+                    const elem2 = document.getElementById("Landing Success_"+landingSuccess);
+                    elem2.classList.remove("btnClassActive");
+                    elem2.classList.add("btnClass");
+                }
+                setLandingSuccess(filterValue);
+                elem.classList.remove("btnClass");
+                elem.classList.add("btnClassActive");
+            }
+        }
+    }
+
+    const filterButtons = (filterName, filterButtonArray) =>{
+        console.log(filterName);
+        return(
+            <Grid style={{textAlign: "center"}}>
+                <span style={{fontWeight: "bold", textDecoration: "underline"}}>
+                    {`${filterName} `}
+                </span>
+                <Grid className="sidePanelElements">
+
+                    {
+                        filterButtonArray.map((item)=>{
+                            return(
+                                <Grid xs={6} sm={6} md={6} lg={6}>
+                                    <button id={`${filterName}_${item}`} className="btnClass" onClick={(event) => buttonSelected(event)}>
+                                        {item}
+                                    </button>
+                                </Grid>
+                            );
+                        })
+                    }
+                </Grid>
+            </Grid>
+
+        );
+    }
+
     return (
         <Paper elevation={3} className={styles.container}>
             <h2>Filters</h2>
+
+            {filterButtons("Launch Year", yearButtons)}
+            {filterButtons("Launch Success", ["True","False"])}
+            {filterButtons("Landing Success", ["True","False"])}
+
             <Grid className="sidePanelElements">
-                <span style={{fontWeight: "bold", textDecoration: "underline"}}>
-                    Launch Year{" "}
-                </span>
-                <div style={{width: 200}}>
-                    <Select
-                        className="basic-single"
-                        classNamePrefix="select"
-                        isClearable
-                        name="year"
-                        onChange={handleChangeYear}
-                        value={{label: year, value: year}}
-                        options={yearArray}
-                    />
-                </div>
-            </Grid>
-            <Grid className="sidePanelElements">
-                <FormControl component="fieldset">
-                    <span style={{fontWeight: "bold", textDecoration: "underline"}}>
-                        Launch Success{" "}
-                    </span>
-                    <RadioGroup
-                        aria-label="launch"
-                        name="launch"
-                        value={launchSuccess}
-                        onChange={handleChangeLaunch}
-                    >
-                        <FormControlLabel value="true" control={<Radio/>} label="True"/>
-                        <FormControlLabel value="false" control={<Radio/>} label="False"/>
-                    </RadioGroup>
-                </FormControl>
-            </Grid>
-            <Grid className="sidePanelElements">
-                <FormControl component="fieldset">
-                    <span style={{fontWeight: "bold", textDecoration: "underline"}}>
-                        Landing Success{" "}
-                    </span>
-                    <RadioGroup
-                        aria-label="landing"
-                        name="landing"
-                        value={landingSuccess}
-                        onChange={handleChangeLanding}
-                    >
-                        <FormControlLabel value="true" control={<Radio/>} label="True"/>
-                        <FormControlLabel value="false" control={<Radio/>} label="False"/>
-                    </RadioGroup>
-                </FormControl>
-            </Grid>
-            <Grid className="sidePanelElements">
-                <Button
-                    variant="contained"
-                    color="primary"
+                <button
+                    className="btnClassActive"
+                    //variant="contained"
+                    //color="primary"
                     onClick={(e) => onClickResetHandler(e, false)}
                 >
                     Reset All
-                </Button>
+                </button>
             </Grid>
             <Grid className="sidePanelElements">
-                <Button
-                    variant="contained"
-                    color="primary"
+                <button
+                    className="btnClassActive"
+                    //variant="contained"
+                    //color="primary"
                     onClick={onClickApplyHandler}
                 >
                     Apply Filter
-                </Button>
+                </button>
             </Grid>
         </Paper>
     );
